bbbbd5179692c9fbbbde2b998f7db1a39fa795df
ingdaeho <ingdaeho@swingby.us>
2024-03-22 19:03:55 +0900

    chore: 환경변수 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+/** @type {import('vite').UserConfig} */
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), TanStackRouterVite()],
+    define: {
+      __API_URL__: JSON.stringify(env.API_URL),
+    },
+  };
 });
