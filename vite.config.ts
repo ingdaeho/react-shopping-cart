30341cde69043053671a708313508bee2be28cc4
ingdaeho <ingdaeho@swingby.us>
2024-03-22 23:24:41 +0900

    chore: @vanilla-css, axios, classnames

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://vitejs.dev/config/
 /** @type {import('vite').UserConfig} */
@@ -8,7 +9,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react(), TanStackRouterVite()],
+    plugins: [react(), TanStackRouterVite(), vanillaExtractPlugin()],
     define: {
       __API_URL__: JSON.stringify(env.API_URL),
     },
