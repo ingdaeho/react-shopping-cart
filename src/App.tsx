f6884cc98e9f9f26b3325a09dba91ef9af5deb67
ingdaeho <ingdaeho@swingby.us>
2024-03-22 19:03:09 +0900

    chore: @tanstack/react-query, @tanstack/react-router

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1>React Shopping Cart</h1>;
-}
-
-export default App;
---
+++ src/routes/__root.tsx
@@ -0,0 +1,19 @@
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <div className='p-2 flex gap-2'>
+        <Link to='/' className='[&.active]:font-bold'>
+          Home
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+      <ReactQueryDevtools buttonPosition='top-right' />
+      <TanStackRouterDevtools position='bottom-right' />
+    </>
+  ),
+});
