0a42329bea74a64c1794bba50eb89e8bf1852fd5
ingdaeho <ingdaeho@swingby.us>
2024-03-30 15:29:21 +0900

    chore: vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 /** @type {import('vite').UserConfig} */
@@ -9,7 +10,12 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react(), TanStackRouterVite(), vanillaExtractPlugin()],
+    plugins: [
+      react(),
+      TanStackRouterVite(),
+      vanillaExtractPlugin(),
+      svgr({ include: '**/*.svg?react' }),
+    ],
     define: {
       __API_URL__: JSON.stringify(env.API_URL),
     },
