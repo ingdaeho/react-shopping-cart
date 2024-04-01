032ac4339c9acc668992d2d9e0e2334741606528
ingdaeho <ingdaeho@swingby.us>
2024-04-01 22:51:04 +0900

    feat: 주문상세페이지 장바구니 모달 추가

--- src/routes/order/$orderId.tsx
+++ src/routes/order/$orderId.tsx
@@ -4,57 +4,70 @@ import { useSuspenseQuery } from '@tanstack/react-query';
 import Button from '../../components/Button/Button';
 import PageTitle from '../../components/PageTitle/PageTitle';
 import HighlightText from '../../components/HighlightText/HighlightText';
+import { useAddToCartMutation } from '../products/-queryOptions';
+import { useModal } from '../../hooks/useModal';
+import CartModal from './-component/CartModal';
 
 export const OrderDetail = () => {
   const { orderId } = Route.useParams();
   const { data } = useSuspenseQuery(orderQueryOptions(orderId));
-  const { id, orderDetails } = data;
+  const { isOpen, openModal, closeModal: handleClose, modalRef } = useModal();
+  const { mutate } = useAddToCartMutation();
 
-  const totalPrice = orderDetails.reduce((acc, cur) => acc + cur.price, 0);
+  const totalPrice = data.orderDetails.reduce((acc, cur) => acc + cur.price, 0);
 
   return (
-    <section className='order-section'>
-      <PageTitle>주문내역상세</PageTitle>
-      <div className='order-list'>
-        <div className='order-list__header'>
-          <span>주문번호: {id}</span>
-        </div>
-        {orderDetails.map(({ id, name, price, imageUrl }) => {
-          return (
-            <div className='order-list-item' key={id}>
-              <div className='flex gap-15 mt-10'>
-                <img className='w-144 h-144' src={imageUrl} alt={name} />
-                <div className='flex-col gap-15'>
-                  <span className='order-name'>{name}</span>
-                  <span className='order-info'>
-                    {price.toLocaleString()}원 / 수량: 3개
-                  </span>
+    <>
+      <section className='order-section'>
+        <PageTitle>주문내역상세</PageTitle>
+        <div className='order-list'>
+          <div className='order-list__header'>
+            <span>주문번호: {data.id}</span>
+          </div>
+          {data.orderDetails.map((product) => {
+            const { id, name, price, imageUrl } = product;
+            return (
+              <div className='order-list-item' key={id}>
+                <div className='flex gap-15 mt-10'>
+                  <img className='w-144 h-144' src={imageUrl} alt={name} />
+                  <div className='flex-col gap-15'>
+                    <span className='order-name'>{name}</span>
+                    <span className='order-info'>
+                      {price.toLocaleString()}원 / 수량: 3개
+                    </span>
+                  </div>
                 </div>
+                <Button
+                  variant='contained'
+                  color='primary'
+                  size='small'
+                  className='flex-center self-start'
+                  onClick={() =>
+                    mutate(product, {
+                      onSuccess: () => openModal(),
+                    })
+                  }
+                >
+                  장바구니
+                </Button>
               </div>
-              <Button
-                variant='contained'
-                color='primary'
-                size='small'
-                className='flex-center self-start'
-              >
-                장바구니
-              </Button>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
 
-      <div className='order-detail-container'>
-        <div className='w-480'>
-          <span className='order-detail-title'>결제금액 정보</span>
-          <hr className='divide-line-thin my-20' />
-          <div className='flex justify-between'>
-            <HighlightText>총 결제금액</HighlightText>
-            <HighlightText>{totalPrice.toLocaleString()}원</HighlightText>
+        <div className='order-detail-container'>
+          <div className='w-480'>
+            <span className='order-detail-title'>결제금액 정보</span>
+            <hr className='divide-line-thin my-20' />
+            <div className='flex justify-between'>
+              <HighlightText>총 결제금액</HighlightText>
+              <HighlightText>{totalPrice.toLocaleString()}원</HighlightText>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <CartModal ref={modalRef} open={isOpen} onClose={handleClose} />
+    </>
   );
 };
 
