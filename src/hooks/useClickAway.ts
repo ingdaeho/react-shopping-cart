9ab2a526748fb68b6856d88812fb16cb0c112227
ingdaeho <ingdaeho@swingby.us>
2024-05-02 17:20:57 +0900

    refactor: useClickAway

--- src/hooks/useClickAway.ts
+++ src/hooks/useClickAway.ts
@@ -2,23 +2,17 @@ import { RefObject, useEffect, useRef } from 'react';
 
 const defaultEvents = ['mousedown', 'touchstart'];
 
-const useClickAway = <E extends Event = Event>(
+const useClickAway = (
   ref: RefObject<HTMLElement | null>,
-  onClickAway: (event: E) => void,
+  onClickAway: () => void,
   events: string[] = defaultEvents
 ) => {
   const savedCallback = useRef(onClickAway);
 
   useEffect(() => {
-    savedCallback.current = onClickAway;
-  }, [onClickAway]);
-
-  useEffect(() => {
     const handler = (event: Event) => {
       const { current: el } = ref;
-      el &&
-        !el.contains(event.target as Node) &&
-        savedCallback.current(event as E);
+      el && !el.contains(event.target as Node) && savedCallback.current();
     };
 
     for (const eventName of events) {
