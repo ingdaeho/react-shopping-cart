d9cb0e1ee6d3ba9fa992b54666bf36e5818db447
ingdaeho <ingdaeho@swingby.us>
2024-05-17 16:46:22 +0900

    chore: vitest, zustand test setting

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,6 @@
+// __mocks__/vitest-env.d.ts
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+/// <reference types="vitest/globals" />
 
 declare const __API_URL__: string;
