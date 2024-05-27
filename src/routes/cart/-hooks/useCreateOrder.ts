7d11e2108a1e65e2d620a24754438d3542f52212
ingdaeho <ingdaeho@swingby.us>
2024-05-27 13:23:21 +0900

    fix: reset selection

--- src/routes/cart/-hooks/useCreateOrder.ts
+++ src/routes/cart/-hooks/useCreateOrder.ts
@@ -2,14 +2,24 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { Order, Product } from '../../../types';
 import fetcher from '../../../lib/axios';
 import { cartItemQueryOptions } from '../-queryOptions';
+import { useCartStore } from '../../../store/cart';
 
 export const useCreateOrder = () => {
   const queryClient = useQueryClient();
+  const resetSelectedItems = () =>
+    useCartStore.setState(() => ({
+      selectedItems: new Set(),
+      isAllSelected: false,
+    }));
+
   return useMutation<Order, Error, Product[]>({
     mutationFn: async (order) => {
       const { data } = await fetcher.post('/orders', order);
       return data;
     },
-    onSettled: () => queryClient.invalidateQueries(cartItemQueryOptions()),
+    onSettled: () => {
+      queryClient.invalidateQueries(cartItemQueryOptions());
+      resetSelectedItems();
+    },
   });
 };
